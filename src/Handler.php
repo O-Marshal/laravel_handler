6ab33195f7159187d7b0976028929130e1146fd7
刘荣 <liurong@ckryo.local>
2017-06-01 10:33:15 +0800

    -

--- src/Handler.php
+++ src/Handler.php
@@ -2,9 +2,12 @@
 
 namespace Ckryo\Laravel\Handler\Exceptions;
 
+use Ckryo\Laravel\Http\Contracts\ExceptionFactory;
+use Ckryo\Laravel\Http\ErrorCodeException;
 use Exception;
-use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -15,11 +18,11 @@ class Handler extends ExceptionHandler
      */
     protected $dontReport = [
         \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
         \Symfony\Component\HttpKernel\Exception\HttpException::class,
         \Illuminate\Database\Eloquent\ModelNotFoundException::class,
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
+        \Ckryo\Laravel\Http\ErrorCodeException::class
     ];
 
     /**
@@ -44,22 +47,40 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($request->isJson()) {
+            switch (true) {
+                /// 默认处理错误
+                case $exception instanceof ExceptionFactory:
+                    return $exception->handle();
+                /// 表单验证错误数据处理方式
+                case $exception instanceof ValidationException:
+                    $errorBag = $exception->validator->errors();
+                    $errors = [];
+                    foreach ($errorBag->messages() as $key => $message) {
+                        $errors[$key] = $message[0];
+                    }
+                    throw new ErrorCodeException(9, $errorBag->first(), $errors);
+                    break;
+                /// 路由不存在 - 404
+                case $exception instanceof MethodNotAllowedHttpException:
+                    throw new ErrorCodeException(21);
+            }
+
+            throw new ErrorCodeException(1, $exception->getMessage(), [
+                $exception->getLine(),
+                $exception->getFile(),
+                $exception->getCode(),
+                $exception->getPrevious(),
+                $exception->getTrace(),
+                $exception->getTraceAsString()
+            ]);
+        }
         return parent::render($request, $exception);
     }
 
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
-    protected function unauthenticated($request, AuthenticationException $exception)
+    // 未授权用户, 需用重新登录的。
+    protected function unauthenticated()
     {
-        if ($request->expectsJson()) {
-            return response()->json(['error' => 'Unauthenticated.'], 401);
-        }
-
-        return redirect()->guest(route('login'));
+        throw new ErrorCodeException(200);
     }
 }
